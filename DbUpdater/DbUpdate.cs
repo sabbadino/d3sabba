393b8f1fba1b73cf05686f16b65c84a6fd22d189
sabbadino <sabbadin12@gmail.com>
2015-06-12 18:18:50 +0200

    dbupdater

---
+++ DbUpdater/DbUpdate.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+using UnityInfrastructure.Logging;
+
+namespace DbUpdater
+{
+	public class DbUpdate
+	{
+		private const string C_VERSIONTABLE ="dbversion";
+		private const string C_SCHEMA = "dbo";
+
+		private const string C_CREATEVERSIONTABLE = @"CREATE TABLE [dbo].[dbVersion] (
+			[ModuleID] [varchar](100) NOT NULL,
+			[Major] [tinyint] NOT NULL,
+			[Minor] [tinyint] NOT NULL,
+			[Build] [tinyint] NOT NULL,
+			[Revision] [smallint] NOT NULL,
+		  [Revision] [date] NOT NULL,
+			[Comment] varchar(MAX) NULL
+		 CONSTRAINT [PK_module_Id] PRIMARY KEY CLUSTERED (	[ModuleID] ASC ) ) ";
+
+		private string _cnString ;
+		private string _scriptsFolder ;
+		private string _moduleId;
+		public DbUpdate(string cnString, string scriptsFolder, string moduleId)
+		{
+			using (var tc = new UnityTraceContext())
+			{
+				_moduleId = moduleId ;
+				_cnString = cnString;
+				_scriptsFolder = scriptsFolder;
+				tc.TraceMessage("_cnString=" + _cnString + " _scriptsFolder=" + _scriptsFolder + " _module=" + _module);
+			}
+		}
+
+		public void Doupdate()
+		{
+			using (var tc = new UnityTraceContext())
+			{
+				if(!Directory.Exists(_scriptsFolder)) throw new Exception("Directory " + _scriptsFolder + " does not exists");
+				using (var ts = new TransactionScope())
+				{
+					using (var cn = new SqlConnection(_cnString))
+					{
+						cn.Open();
+						Version ldbCurVer = getDbVersion(cn);
+						var scriptsInfo = loadScriptInfo(_scriptsFolder);
+						var scriptsInfoToExecute = scriptsInfo.Where(v => v.Version > ldbCurVer).ToList();
+						if (scriptsInfoToExecute.Count > 0)
+						{
+							executeScripts(scriptsInfoToExecute, cn);
+							updateDbVersion(cn, scriptsInfoToExecute.Last());
+						}
+					}
+					ts.Complete();
+				}
+			}
+		}
+
+		private void updateDbVersion(SqlConnection cn, ScriptInfo scriptInfo)
+		{
+			using (var tc = new UnityTraceContext())
+			{
+				var cmd = cn.CreateCommand();
+				cmd.CommandText = String.Format(C_UpdateVersion, scriptInfo.Version.Major, scriptInfo.Version.Minor, scriptInfo.Version.Build, scriptInfo.Version.Revision);
+				cmd.ExecuteNonQuery();
+			}
+		}
+
+		private void executeScripts(List<ScriptInfo> scriptsInfoToExecute, SqlConnection cn)
+		{
+			using (var tc = new UnityTraceContext())
+			{
+				scriptsInfoToExecute.ForEach(script =>
+				{
+					var cmd = cn.CreateCommand();
+					cmd.CommandText = File.ReadAllText(script.Path);
+					cmd.ExecuteNonQuery();
+				});
+			}
+		}
+
+		private IList<ScriptInfo> loadScriptInfo( string folderPath)
+		{
+			var x = new List<ScriptInfo>();
+			x.Sort();
+			return x;
+		}
+
+		private Version getDbVersion(SqlConnection cn)
+		{
+			using (var tc = new UnityTraceContext())
+			{
+				var ver = new Version(0, 0, 0, 0);
+				var lcmd = cn.CreateCommand();
+				lcmd.CommandText = "SELECT count(*) FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = '" + C_SCHEMA + "' AND  TABLE_NAME = '" + C_VERSIONTABLE + "'";
+				if ((int) lcmd.ExecuteScalar() == 0)
+				{
+					createVersionTable(cn);
+				}
+				else
+				{
+					lcmd.CommandText = "SELECT top 1 from " + C_SCHEMA + "." + C_VERSIONTABLE + " where moduleid=@moduleid order by maior,minor,build,revision desc";
+					var parammoduleid = lcmd.CreateParameter();
+					parammoduleid.ParameterName = "moduleid";
+					parammoduleid.Value = _moduleId;
+					lcmd.Parameters.Add(parammoduleid);
+					var rd = lcmd.ExecuteReader();
+					if (rd.HasRows)
+					{
+						ver = new Version((int) rd["maior"], (int) rd["minor"], (int) rd["build"], (int) rd["revision"]);
+					}
+				}
+				tc.TraceMessage("dbVersion=" + ver.ToString());
+				return ver;
+			}
+		}
+
+		private void createVersionTable(SqlConnection cn)
+		{
+			using (var tc = new UnityTraceContext())
+			{
+				var lcmd = cn.CreateCommand();
+				lcmd.CommandText = C_CREATEVERSIONTABLE;
+				lcmd.ExecuteNonQuery();
+			}
+		}
+	}
+
+	public class ScriptInfo : IComparable<Version> 
+	{
+		public string Path;
+		public Version Version;
+		public string Name;
+
+		public int CompareTo(Version other)
+		{
+			return this.Version.CompareTo(other);
+		}
+	}
+}
---
+++ DbUpdater/UnityTraceContext.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Globalization;
+using System.Reflection;
+using System.Text;
+using log4net;
+using log4net.Core;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+
+
+namespace UnityInfrastructure.Logging
+{
+	public sealed class UnityTraceContext : IUnityTraceContext
+	{
+		#region Fields
+
+		private static readonly ILogger _Logger;
+
+		private const char DEFAULTVALUE_TRACE_NESTING_STRING = ' ';
+
+		private readonly DateTime _startTime = DateTime.Now;
+		private string _className, _procedureName;
+		private Assembly _assembly;
+		private string _assemblyName;
+		private Type _type;
+		private int _nestingLevel;
+		[ThreadStatic]
+		private static int _CurrentNestingLevel;
+
+		private const string UNKNOWN_CLASS = "unknownclass";
+		private const short ASSEMBLY_NAME_MAX_LENGTH = 50;
+
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", MessageId = "UNITYEXCEPTIONSTACKTRACE")]
+		public const string UNITYEXCEPTIONSTACKTRACE = "UnityExceptionStackTrace";
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", MessageId = "UNITYEXCEPTIONTYPE")]
+		public const string UNITYEXCEPTIONTYPE = "UnityExceptionStackTrace";
+
+		//private Dictionary<string, object> m_extrainfo = new Dictionary<string, object>();
+		//private string m_extrainfo = "";
+		private bool _writeEnterExitLogrows = true;
+
+		private Level _defaultLevel = Level.Debug;
+
+		private int _StackTrRaceUp = 2;
+
+		#endregion
+
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA2204:Literals should be spelled correctly", MessageId = "GenyaTraceContext")]
+		static UnityTraceContext()
+		{
+			try
+			{
+				_Logger = LogManager.GetLogger("DbUpdater").Logger;
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("static GenyaTraceContext=" + ex);
+				Debug.WriteLine("static GenyaTraceContext=" + ex);
+				throw;
+			}
+		}
+
+
+
+
+		private static void LogEntrySafe(string msg, Level level)
+		{
+			try
+			{
+				_Logger.Log(typeof(UnityTraceContext), level, msg, null);
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("LogEntrySafe=" + ex);
+				Debug.WriteLine("LogEntrySafe=" + ex);
+			}
+		}
+
+		private readonly static string _Machiname = Environment.MachineName;
+		private readonly static string _ProcessId = Process.GetCurrentProcess().Id.ToString(CultureInfo.InvariantCulture);
+
+		private static void addBuiltin(IDictionary dictionary)
+		{
+			dictionary.Add("tid", Thread.CurrentThread.ManagedThreadId);
+			dictionary.Add("MachineName", _Machiname);
+			dictionary.Add("pid", _ProcessId);
+		}
+
+
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes")]
+		private void processEnter(bool writeEnterExitLogrows, Level defaultLevel, Assembly assembly, Type type, string procedureName)
+		{
+			try
+			{
+				_writeEnterExitLogrows = writeEnterExitLogrows;
+				_defaultLevel = defaultLevel;
+
+				if (assembly != null)
+				{
+					_assembly = assembly;
+				}
+				if (type != null)
+				{
+					_type = type;
+				}
+				if (procedureName != null)
+				{
+					_procedureName = procedureName;
+				}
+
+				//bool bIsStatic = false;
+				if (_assembly == null || _type == null || _procedureName == null)
+				{
+					System.Diagnostics.StackTrace st = null;
+					System.Diagnostics.StackFrame sf = null;
+					MethodBase mb = null;
+					st = new System.Diagnostics.StackTrace();
+					if (st != null)
+					{
+						sf = st.GetFrame(_StackTrRaceUp);
+					}
+					if (sf != null)
+					{
+						mb = sf.GetMethod();
+					}
+					if (mb != null)
+					{
+						//bIsStatic = mb.IsStatic;
+						if (_assembly == null)
+						{
+							_assembly = mb.Module.Assembly;
+						}
+						if (_type == null)
+						{
+							_type = mb.DeclaringType;
+						}
+						if (_procedureName == null)
+						{
+							_procedureName = mb.Name;
+						}
+					}
+				}
+
+				if (_type != null)
+				{
+					_className = _type.Name;
+				}
+				if (_className == null)
+				{
+					if (_procedureName.Contains("."))
+					{
+						string[] _aProcedureName = _procedureName.Split('.');
+						_className = _aProcedureName[0];
+						_procedureName = _aProcedureName[1];
+					}
+					else
+					{
+						_className = UNKNOWN_CLASS;
+					}
+				}
+
+				_nestingLevel = _CurrentNestingLevel;
+
+				_assemblyName = _assembly.GetName().Name.PadRight(ASSEMBLY_NAME_MAX_LENGTH);
+				if (_assemblyName.Length > ASSEMBLY_NAME_MAX_LENGTH)
+					_assemblyName = _assemblyName.Substring(_assemblyName.Length - ASSEMBLY_NAME_MAX_LENGTH);
+				if (_writeEnterExitLogrows)
+				{
+					string lEnterMessage = buildMessage("-> " + _className + "." + _procedureName);
+					LogEntrySafe(lEnterMessage, defaultLevel);
+					_nestingLevel = _CurrentNestingLevel += 1;
+				}
+			}
+			catch (Exception ex)
+			{
+				Debug.WriteLine(ex.ToString());
+			}
+		}
+
+		private string buildMessage(string pRowMessage)
+		{
+			var lMessage = new StringBuilder();
+			lMessage.Append(_assemblyName + "\t");
+			lMessage.Append(DEFAULTVALUE_TRACE_NESTING_STRING, _nestingLevel);
+			lMessage.Append(pRowMessage);
+			return lMessage.ToString();
+		}
+
+
+		public UnityTraceContext()
+		{
+			// DO NOT REFACTOR de different constrctors overload : processEnter suppose to be a fixed number of steps down the caller of the constructor
+			processEnter(true, Level.Debug, null, null, null);
+		}
+
+		public UnityTraceContext(bool writeEnterExitLogRows)
+		{
+			processEnter(writeEnterExitLogRows, Level.Debug, null, null, null);
+		}
+
+		public UnityTraceContext(bool writeEnterExitLogRows, Level defaultLevel, Assembly callingAssembly, Type callingType, string procedureName)
+		{
+			processEnter(writeEnterExitLogRows, defaultLevel, callingAssembly, callingType, procedureName);
+		}
+
+
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA2204:Literals should be spelled correctly", MessageId = "TraceMessage"), System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes")]
+		public void TraceMessage([Localizable(false)] string message, Level level)
+		{
+			try
+			{
+				var msg = buildMessage(message);
+				LogEntrySafe(msg, level);
+			}
+			catch (Exception ex1)
+			{
+				Console.WriteLine("TraceMessage=" + ex1.ToString());
+				Debug.WriteLine("TraceMessage=" + ex1.ToString());
+			}
+		}
+
+		public void TraceMessage([Localizable(false)] string message)
+		{
+			TraceMessage(message, _defaultLevel);
+		}
+
+
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA2204:Literals should be spelled correctly", MessageId = "TraceError"), System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes")]
+		public void TraceError(Exception ex, Level level)
+		{
+			try
+			{
+				if (ex == null) 
+					return;
+				
+				LogEntrySafe(ex.Message, level);
+			}
+			catch (Exception ex1)
+			{
+				Console.WriteLine("TraceError=" + ex1.ToString());
+				Debug.WriteLine("TraceError=" + ex1.ToString());
+			}
+		}
+
+		public void TraceError(Exception ex)
+		{
+			TraceError(ex, _defaultLevel);
+		}
+
+
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes")]
+		public void Dispose()
+		{
+			try
+			{
+				if (_writeEnterExitLogrows)
+				{
+					_nestingLevel = _CurrentNestingLevel -= 1;
+					string lExitMessage =
+						buildMessage("<- " + "( " + (DateTime.Now - _startTime).TotalSeconds + " .sec) " + _className + "." +
+												 _procedureName);
+					var entry = lExitMessage;
+					LogEntrySafe(entry, _defaultLevel);
+				}
+			}
+			catch (Exception ex)
+			{
+				Debug.WriteLine(ex.ToString());
+			}
+		}
+	}
+
+
+	public interface IUnityTraceContext : IDisposable
+	{
+		void TraceMessage(string message, Level level);
+		void TraceMessage(string message);
+		void TraceError(Exception ex, Level level);
+		void TraceError(Exception ex);
+	}
+}
