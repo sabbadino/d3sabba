1da81810715c390659b197a01b32e0adc5c1b33c
sabbadino <sabbadin12@gmail.com>
2015-08-06 08:57:08 +0200

    ffffff

---
+++ SS_ASPNET_01/SS_ASPNET_01/SS_ASPNET_01.ServiceModel/Types/FindDate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using ServiceStack;
+
+namespace SS_ASPNET_01.ServiceModel
+{
+	[Route("/dates", "GET")]
+	[DataContract]
+	public class FindDate : IReturn<FindDateResponse>
+	{
+		[DataMember(Name = "DateTime")]
+		public DateTime DateTime { get; set; }
+	}
+
+	[DataContract]
+	public class FindDateResponse : IHasResponseStatus
+	{
+		[DataMember(Name = "Dates")]
+		public List<DateTime> Dates { get; set; }
+
+		ResponseStatus _ResponseStatus = new ResponseStatus();
+
+		public ResponseStatus ResponseStatus
+		{
+			get { return _ResponseStatus; }
+			set { _ResponseStatus = value; }
+		}
+	}
+}
